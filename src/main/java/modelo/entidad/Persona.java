fa64de3dc76a8aa36c40ae0b36b43e6b811b2fec
JCasasChacaliaza <james.casas@vallegrande.edu.pe>
2019-09-18 19:44:15 -0500

    CRUD completado

--- src/main/java/modelo/entidad/Persona.java
+++ src/main/java/modelo/entidad/Persona.java
@@ -12,12 +12,15 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
@@ -46,8 +49,9 @@ public class Persona implements Serializable {
     private static final long serialVersionUID = 1L;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
     @Id
-    @Basic(optional = false)
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+//    @Null
     @Column(name = "IDPER")
     private BigDecimal idper;
     @Basic(optional = false)
